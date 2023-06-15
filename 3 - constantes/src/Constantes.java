150398da9be01f6e03bf78cad4822b6465be1ba0
Sidney Cardoso <sidneyrsc342@gmail.com>
2023-06-15 12:26:23 -0300

    Constantes

---
+++ 3 - constantes/src/Constantes.java
@@ -0,0 +1,17 @@
+public class Constantes {
+    public static void main(String[] args) {
+
+        final float PI = 3.146f;
+        final double E = 1.234e2;
+
+        String nome = "João Pedro";
+        int lote = 75825;
+        boolean aprovado = true;
+
+        System.out.println(PI);
+        System.out.println(E);
+        System.out.println(nome);
+        System.out.println(lote);
+        System.out.println(aprovado);
+    }
+}
